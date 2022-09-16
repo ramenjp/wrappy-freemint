02dfbdbef44f43d469787db8a8395fc9b98f40e5
ramenjp <takagiyousuke5@gmail.com>
2022-09-16 11:18:29 +0900

    fix import

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -5,7 +5,7 @@ import { getDefaultProvider } from "ethers";
 import { Template } from "../templates";
 import { Contract } from "@ethersproject/contracts";
 import abi from "../abi.json";
-import ethers from "ethers";
+import { utils } from "ethers";
 
 const Home: NextPage = () => {
   const config = {
@@ -18,13 +18,14 @@ const Home: NextPage = () => {
   const { activateBrowserWallet, chainId, account } = useEthers();
 
   const contractAddress = "0x7b98de4493979474cffbe1ab6cd45a72450b4cc0";
-  const contractInterface = new ethers.utils.Interface(abi);
+  const contractInterface = new utils.Interface(abi);
   const contract = new Contract(contractAddress, contractInterface);
 
-  const { state, send } = useContractFunction(contract, "deposit", {
+  const { state, send } = useContractFunction(contract, "mintedMap", {
     transactionName: "Wrap",
     gasLimitBufferPercentage: 10,
   });
+  console.log("state :", state);
   React.useEffect(() => {}, []);
 
   return (
