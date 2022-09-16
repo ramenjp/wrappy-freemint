0ab142329feed80b93467fc7450b516f40847698
ramenjp <takagiyousuke5@gmail.com>
2022-09-16 11:13:08 +0900

    wip

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -1,8 +1,11 @@
 import * as React from "react";
 import type { NextPage } from "next";
-import { useEthers, Mainnet, Goerli } from "@usedapp/core";
+import { useEthers, Mainnet, Goerli, useContractFunction } from "@usedapp/core";
 import { getDefaultProvider } from "ethers";
 import { Template } from "../templates";
+import { Contract } from "@ethersproject/contracts";
+import abi from "../abi.json";
+import ethers from "ethers";
 
 const Home: NextPage = () => {
   const config = {
@@ -14,6 +17,14 @@ const Home: NextPage = () => {
 
   const { activateBrowserWallet, chainId, account } = useEthers();
 
+  const contractAddress = "0x7b98de4493979474cffbe1ab6cd45a72450b4cc0";
+  const contractInterface = new ethers.utils.Interface(abi);
+  const contract = new Contract(contractAddress, contractInterface);
+
+  const { state, send } = useContractFunction(contract, "deposit", {
+    transactionName: "Wrap",
+    gasLimitBufferPercentage: 10,
+  });
   React.useEffect(() => {}, []);
 
   return (
